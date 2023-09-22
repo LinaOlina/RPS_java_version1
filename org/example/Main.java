3523c20b43ddeb89486453726511f24a80c785a8
CaptainMorgan1992 <kimberly.morgan1992@outlook.com>
2023-09-22 10:57:38 +0200

    Check

--- org/example/Main.java
+++ org/example/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Hej");
+        System.out.println("Check");
         }
 
 }
